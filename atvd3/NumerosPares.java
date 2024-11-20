8f64a6eb3d338425c3c46cc7e86374ca5e47412b
LucasPdjl <168298658+LucasPdjl@users.noreply.github.com>
2024-11-20 12:47:46 -0300

    Create NumerosPares.java

---
+++ atvd3/NumerosPares.java
@@ -0,0 +1,22 @@
+class NumerosPares implements Runnable {
+    private final Object lock; // Objeto q vai fazer a sincronização ***
+
+    public NumerosPares(Object lock) {
+        this.lock = lock;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 2; i <= 10; i += 2) {
+            synchronized (lock) { // Bloqueia o recurso compartilhado ***
+                System.out.println(i);
+                lock.notify(); // "chama" a utra thread
+                try {
+                    lock.wait(); 
+                } catch (InterruptedException e) {
+                    e.printStackTrace(); // interrupções ******************
+                }
+            }
+        }
+    }
+}
