6e29a419f92d30b65a80a5a03d7d7858e0a4e760
LucasPdjl <168298658+LucasPdjl@users.noreply.github.com>
2024-11-20 12:51:22 -0300

    Update NumerosPares.java

--- atvd3/NumerosPares.java
+++ atvd3/NumerosPares.java
@@ -8,15 +8,16 @@ class NumerosPares implements Runnable {
     @Override
     public void run() {
         for (int i = 2; i <= 10; i += 2) {
-            synchronized (lock) { // Bloqueia o recurso compartilhado ***
+            synchronized (lock) { // Bloqueia o recurso compartilhado *******
                 System.out.println(i);
                 lock.notify(); // "chama" a utra thread
                 try {
                     lock.wait(); 
                 } catch (InterruptedException e) {
-                    e.printStackTrace(); // interrupções ******************
+                    e.printStackTrace(); // interrupções ***
                 }
             }
+            lock.notify(); 
         }
     }
 }
